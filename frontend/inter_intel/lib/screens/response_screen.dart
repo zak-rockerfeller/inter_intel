9055afdc6129dc00eeaa4ed2e0e8f5e1da9c8548
Isaac Karanja <55271909+zak-rockerfeller@users.noreply.github.com>
2023-01-11 13:54:43 +0300

    Update response_screen.dart

--- frontend/inter_intel/lib/screens/response_screen.dart
+++ frontend/inter_intel/lib/screens/response_screen.dart
@@ -1,9 +1,5 @@
-import 'dart:ui';
 import 'package:animate_gradient/animate_gradient.dart';
-import 'package:animate_gradient/animate_gradient.dart';
-import 'package:clay_containers/constants.dart';
-import 'package:clay_containers/widgets/clay_container.dart';
-import 'package:customizable_space_bar/customizable_space_bar.dart';
+import 'package:inter_intel/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:inter_intel/models/models.dart';
@@ -65,6 +61,7 @@ class _ResponseScreenState extends State<ResponseScreen> with SingleTickerProvid
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       body: AnimateGradient(
         primaryBegin: Alignment.topLeft,
         primaryEnd: Alignment.bottomLeft,
@@ -87,7 +84,9 @@ class _ResponseScreenState extends State<ResponseScreen> with SingleTickerProvid
               pinned: true,
               leading: IconButton(
                 icon: const Icon(FontAwesomeIcons.arrowLeftLong, color: Colors.blue,),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) => NavScreen(selectedIndex: 1,),),);
+                },
               ),
               backgroundColor: Theme.of(context).scaffoldBackgroundColor,
             ),
@@ -106,47 +105,34 @@ class _ResponseScreenState extends State<ResponseScreen> with SingleTickerProvid
                           blurRadius: 6.0)
                     ],
                   ),
-                  child: ListView.builder(
+                  child: userList.isEmpty ?
+                  const Center(child: CircularProgressIndicator()) :
+                  ListView.builder(
                     scrollDirection: Axis.vertical,
                     itemCount: userList.length,
                     itemBuilder: (BuildContext context, int index) {
                       ToDo popular = userList[index];
-                      return Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20.0),
-                              child:BackdropFilter(
-                                filter: ImageFilter.blur(
-                                  sigmaX: 33,
-                                  sigmaY: 65,
-                                ),
-                                child: Container(
-                                  //height: 90,
-                                  width: MediaQuery.of(context).size.width * 0.9,
-                                  padding: const EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                      color: Colors.black45,
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      border: Border.all(width: 1.5, color: Colors.white.withOpacity(0.2))
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(popular.title!,style: const TextStyle(
-                                        color: Colors.white, fontSize: 18.0,fontWeight: FontWeight.bold,letterSpacing: 0.7,),),
-                                      const SizedBox(height: 5,),
-                                      Text(popular.completed.toString()!,style: const TextStyle(
-                                        color: Colors.white, fontSize: 14.0,fontWeight: FontWeight.w500,letterSpacing: 0.5,),),
-
-                                    ],),
-                                ),
-                              ),
-
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 5),
+                        child: Container(
+                          //height: 80,
+                          padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                             color: Colors.black12,
+                             borderRadius: BorderRadius.circular(20.0),
+                             border: Border.all(width: 1.5, color: Colors.white.withOpacity(0.2))
+                                    ),
+                          child: ListTile(
+                            minVerticalPadding: 5,
+                            contentPadding: const EdgeInsets.all(10),
+                            leading: const CircleAvatar(
+                              radius: 20,
+                              backgroundImage: AssetImage("assets/images/logo.png"),
                             ),
+                            title: Text(popular.title.toString(),style: const TextStyle(fontSize: 18.0,fontWeight: FontWeight.w500,letterSpacing: 0.3,),),
+                            trailing: Text(popular.completed.toString(),style: const TextStyle(fontSize: 15.0,fontWeight: FontWeight.w400,letterSpacing: 0.5,),),
                           ),
-                        ],
+                        ),
                       );
                     },
                   ),
